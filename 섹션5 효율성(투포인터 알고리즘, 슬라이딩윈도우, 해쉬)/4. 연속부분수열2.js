0fa9c4190f6f56a3a6547f1503c94f7f21cc5d9e
Sungyun <qwert_yyy@naver.com>
2022-04-01 23:41:09 +0900

    solve : section 5-3

--- 섹션5 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/4. 연속부분수열2.js
+++ 섹션5 효율성(투포인터 알고리즘, 슬라이딩윈도우, 해쉬)/4. 연속부분수열2.js
@@ -1,5 +1,23 @@
 function solution(m, arr) {
-  let answer;
+  let answer = [],
+    p = 0,
+    q = 0,
+    sum = 0,
+    n = arr.length,
+    subArr = [];
+
+  while (q < n && p < n) {
+    sum += arr[q];
+    if (sum <= m) {
+      subArr.push(arr[q++]);
+      answer.push(subArr);
+    } else {
+      subArr = [];
+      p++;
+      q = p;
+      sum = 0;
+    }
+  }
   return answer;
 }
 
